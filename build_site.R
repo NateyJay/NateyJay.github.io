ef63baca28bdfd5cf95fc12966f10548822a2f4e
Nate Johnson <nrj113@Sonora.bio.psu.edu>
2019-06-11 17:16:39 -0400

    Initial commit

---
+++ build_site.R
@@ -0,0 +1,9 @@
+#Set our working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+
+setwd("/Volumes/Keep/+google_drive/++personal_website/NateyJay.github.io")
+
+#render your sweet site. 
+rmarkdown::render_site()
